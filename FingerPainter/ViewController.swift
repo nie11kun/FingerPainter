01f0569987c450a69e9486ff506d4adc633e51e1
Marco Nie <nie11kun@outlook.com>
2017-05-19 08:42:21 +0800

    update

--- FingerPainter/ViewController.swift
+++ FingerPainter/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvas: UIImageView!
+    
+    @IBAction func clearCanvas(_ sender: UIBarButtonItem) {
+        canvas.image = nil
+    }
+
+    var start: CGPoint?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            start = touch.location(in: canvas)
+        }
+    }
 
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let end = touch.location(in: canvas)
+            if let start = self.start {
+                drawFromPoint(start: start, toPoint: end)
+            }
+            self.start = end
+        }
+    }
+    
+    func drawFromPoint(start: CGPoint, toPoint end: CGPoint) {
+        UIGraphicsBeginImageContext(canvas.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        canvas.image?.draw(in: CGRect(x: 0, y: 0,
+                                      width: canvas.frame.size.width, height: canvas.frame.size.height))
+        context?.setStrokeColor(UIColor.magenta.cgColor)
+        context?.setLineWidth(5)
+        context?.beginPath()
+        context?.move(to: start)
+        context?.addLine(to: end)
+        context?.strokePath()
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        canvas.image = newImage
+    }
+    
 }
 
